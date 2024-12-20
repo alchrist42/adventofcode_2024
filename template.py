e527abdb2c0bb7edd6206ae0cc5856512120ab2d
arh <arheyy@gmail.com>
2024-12-20 17:52:18 +0300

    iupdate template.py

--- template.py
+++ template.py
@@ -1,10 +1,12 @@
+import sys, os
 from pathlib import Path
-from collections import Counter
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import nbs
 
 
 # part 1
-def solve(data: list[str]):
+def solve(data: list[list[str]]):
     res = 0
 
     return res
@@ -14,14 +16,10 @@ def solve(data: list[str]):
 
 
 
-path = Path(__file__)
-filename = path.name.rstrip(".py")
-
-with open(f"{filename}_temp.txt") as f:
-    example_data = [line.rstrip("\n") for line in f.readlines() if line.rstrip()]
-with open(f"{filename}.txt") as f:
-    my_data = [line.rstrip("\n") for line in f.readlines() if line.rstrip()]
-
-if example_data:
-    print(f"Example answer: {solve(example_data)}\n")
-print(solve(my_data))
+for file, want, *args in (("test", 51), ("input", None)):
+    with open(f"{Path(__file__).parent}/{file}.txt") as f:
+        blocks = [block.split("\n") for block in f.read().split("\n\n")]
+    print(f"---- {file} -----\nData shapes:", [(len(b), len(b[0])) for b in blocks])
+    res = solve(blocks, *args)
+    assert want is None or res == want, f"{res}, expected {want}"
+    print(f"Result:", res)
